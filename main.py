e247003c0553bbe5a318e37108473211fb478dd4
OleksiakJ <100289875+OleksiakJ@users.noreply.github.com>
2022-03-01 22:21:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
 import sys
 import math
-#
-# print(sys.version)
-#
-# a='inżynier\n systemów\n informatycznych\n'
-# print(a)
-#
-# print(type(a))
-#
-# b = 5
-#
-# print(b)
-# print(type(b))
-#
-# c=6.5
-#
-# print(c)
-# print(type(c))
-#
-# d = 2+3j
-# print(d)
-# print(type(d))
-#
-# #nowa_podloga
-#
-# # dfgsdfger
-# # ergdfgdfg ctrl+/
-# # del a <<<<usuwanie zmiennej
-# # print(a)<<<< juz sie nie pokaze bo jest usunieta
-#
-# f='isi '#spacja aby mozna bylo oddzielic wyrazy
-# g='grupa 3'
-# print(f+g)
-# h=7
-# i=2
-# print(h//i)#dzielenie calkowite
-# print(h**i)#potegowanie h-potega i- wykladnik potegi **-potegowanie
-#
-# print(math.pow(h, i))#potegowanie z biblioteki math typ float
-#  #ulamki zapisywac w (nawiasach)
-#
-#  h += 1
-#  print(h)
 
-a = 5
-b = 3
-c = a - b
-print('wynik działania %(z1)d - %(z2)d = %(z3)d'%{'z1':a , 'z2': b , 'z3': c})
-print("wynik z działania {0:d} - {1:d} = {2:d}".format (a, b, c))
+# Zad 1
+
+a = 1
+b = 2
+print(a, b)
+c = 4.4
+d = 5.5
+print(c, d)
+e = "pycharm"
+f = "fajny"
+print(e+" "+f)
+g = 5j
+h = 6 + 3.8j
+print(g, h)
+del a, b, c, d, e, f, g, h
+
+# Zad 2
+
+a = int(input("Podaj pierwsza liczbe: "))
+b = int(input("Podaj druga liczbe: "))
+print("1.Dodawanie 2.Odejmowanie 3.Mnożenie 4.Dzielenie 5.Potęgownie: ")
+c = int(input())
+if c == 1:
+    print(a+b)
+elif c == 2:
+    print(a-b)
+elif c == 3:
+   print(a*b)
+elif c == 4:
+    print(a/b)
+elif c == 5:
+    print(pow(a, b))
+else:
+    print("nieprawidlowa wartosc")
+del a, b, c
+
+# Zad 3
+
+a = 2
+a += 2
+print(a)
+b = 10
+b -= 8
+print(b)
+c = 5
+c *= 4
+print(c)
+d = 100
+d /=25
+print(d)
+e = 2
+e **= 10
+print(e)
+f = 11
+f %= 3
+print(f)
+del a, b, c, d, e, f
+
+# Zad 4
+
+print(math.e**10)
+
+print(pow(math.log(5 + (math.sin(8)) ** 2, math.e), 1/6))
+
+print(math.floor(3.55))
+
+print(math.ceil(4.80))
